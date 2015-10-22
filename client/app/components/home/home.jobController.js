820f729e1319ab0656e340a2ae84a78d24a2926d
unknown <ccrain@TCH-69-70-S97.artnet.local>
2015-10-22 16:33:01 -0400

    made the job detail page get data from backend

--- client/app/components/home/home.jobController.js
+++ client/app/components/home/home.jobController.js
@@ -1,8 +1,13 @@
 var app = angular.module('app');
 
 app.controller('JobCtrl',function($scope,$http,$stateParams,$state){
-	$scope.detailJob = $scope.selectedJob;
-	$scope.dimensions = Object.keys($scope.detailJob.boardDimensions);
+	$http.get('http://localhost:3000/api/repair-jobs/'+$scope.selectedJob._id)
+			.then(function(res){
+				$scope.detailJob = res.data;
+				$scope.dimensions = Object.keys($scope.detailJob.boardDimensions);
+			}, function(error){
+				console.log(error);
+			});
 
 	$scope.updateJob = function(job){
 		console.log('happening');
@@ -10,8 +15,9 @@ app.controller('JobCtrl',function($scope,$http,$stateParams,$state){
 		$http.put('http://localhost:3000/api/repair-jobs/'+job._id,job)
 			.then(function(res){
 				console.log(res);
+				$state.go('home',{}, {reload:true});
 			}, function(error){
-				
+				$state.go('home',{}, {reload:true});
 			});
 	};
 	$scope.deleteJob = function(job){
